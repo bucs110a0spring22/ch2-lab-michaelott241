4ef39a7438095cf7d0fbe59ec34af0b9d883de67
Michael O P <98827447+michaelott241@users.noreply.github.com>
2022-02-14 05:34:26 +0000

    Variable Addition and List Construction

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-
+import random
 #Part A
 weeks = 16
 classes = 5
 tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
-
-
+classes_per_week = 3
+cost_per_class = (cost_per_week / classes_per_week)
+print(cost_per_class)
+print(cost_per_class, type(cost_per_class))
+print(classes_per_week, type(classes_per_week))
+print(weeks, type(weeks))
+print(classes, type(classes))
+print(tuition, type(tuition))
+print(cost_per_week, type(cost_per_week))
 #Part B
+Apple = [1,2,3,4,5]
+Orange = random.choice(Apple)
+print(Orange)
